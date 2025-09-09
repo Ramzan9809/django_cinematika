691f3cfbbc50a9227ff3f5fa7f47af741e7ad45c
Ramzan <ramzan.rgwctx9809@gmail.com>
2025-09-09 18:18:29 +0600

    Lesson 2

--- films/admin.py
+++ films/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from .models import Film
+from .models import Film, Director, Genre, Review
 
 
-admin.site.register(Film)
+class ReviewInline(admin.StackedInline):
+    model = Review
+    extra = 0
+
+
+class FilmAdmin(admin.ModelAdmin):
+    inlines = [ReviewInline]
+    list_display = 'title director rating_kp created updated'.split()
+    search_fields = 'title text'.split()
+    list_filter = 'director genres'.split()
+
+
+admin.site.register(Film, FilmAdmin)
+admin.site.register(Director)
+admin.site.register(Genre)
--- films/models.py
+++ films/models.py
@@ -1,9 +1,31 @@
 from django.db import models
+from datetime import datetime
 
+
+class Director(models.Model):
+    fio = models.CharField(max_length=255)
+    birthday = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.fio
+
+    def age(self):
+        now = datetime.now()
+        return now.year - self.birthday.year
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
 
 class Film(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField(null=True, blank=True)
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, null=True)
+    genres = models.ManyToManyField(Genre)
     rating_kp = models.FloatField()
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -11,4 +33,16 @@ class Film(models.Model):
 
     def __str__(self):
         return self.title
+    
 
+STARS = ((i, '*' * i) for i in range(1, 11))
+
+
+class Review(models.Model):
+    text = models.TextField()
+    film = models.ForeignKey(Film, on_delete=models.CASCADE,
+                             related_name='reviews')
+    stars = models.IntegerField(choices=STARS, default=7)
+
+    def __str__(self):
+        return self.text
