158addd011f75bf4362e7bea4cad752fd9697fe6
Ramzan <ramzan.rgwctx9809@gmail.com>
2025-09-19 16:47:34 +0600

    Lesson 5

--- cinematika/settings.py
+++ cinematika/settings.py
@@ -39,7 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'films',
     'rest_framework',
+    'users',
+    'rest_framework.authtoken',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
